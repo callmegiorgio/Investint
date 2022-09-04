aeb6ff137b4098d7586d78323fd4859f7a320722
Giovanni L <callmegiorgio@hotmail.com>
2022-09-04 10:35:48 -0300

    use __all__ in package 'database'

--- investint/database/engine.py
+++ investint/database/engine.py
@@ -1,49 +1,58 @@
-import sqlalchemy      as _sa
-import sqlalchemy.pool as _sa_pool
+import sqlalchemy      as sa
+import sqlalchemy.pool as sa_pool
 
-def createEngineFromUrl(url: _sa.engine.URL, **kwargs) -> _sa.engine.Engine:
+__all__ = [
+    'createEngineFromUrl',
+    'createEngineInMemory',
+    'createEngineFromFile',
+    'isSqliteEngine',
+    'isInMemoryEngine',
+    'isFileEngine'
+]
+
+def createEngineFromUrl(url: sa.engine.URL, **kwargs) -> sa.engine.Engine:
     """Creates a SQLAlchemy engine from an URL.
     
     Note that the engine is always created to use the future API,
     and echoes if the program is running in debug mode, that is,
     if `__debug__` is True.
 
-    Raises `_sa.SQLAlchemy` if engine failed to be created.
+    Raises `sa.SQLAlchemy` if engine failed to be created.
     Otherwise, returns a new engine.
     """
 
     kwargs['future'] = True
     kwargs['echo']   = bool(__debug__)
 
-    return _sa.create_engine(url, **kwargs)
+    return sa.create_engine(url, **kwargs)
 
-def createEngineInMemory() -> _sa.engine.Engine:
+def createEngineInMemory() -> sa.engine.Engine:
     """Creates an engine wrapping an in-memory SQLite connection."""
 
     return createEngineFromUrl(
-        _sa.engine.URL.create('sqlite'),
+        sa.engine.URL.create('sqlite'),
         connect_args={'check_same_thread': False},
-        poolclass=_sa_pool.StaticPool
+        poolclass=sa_pool.StaticPool
     )
 
-def createEngineFromFile(file_path: str) -> _sa.engine.Engine:
+def createEngineFromFile(file_path: str) -> sa.engine.Engine:
     """Creates an engine wrapping a SQLite connection associated with a file."""
 
-    return createEngineFromUrl(_sa.engine.URL.create('sqlite', database=file_path))
+    return createEngineFromUrl(sa.engine.URL.create('sqlite', database=file_path))
 
-def isSqliteEngine(engine: _sa.engine.Engine) -> bool:
+def isSqliteEngine(engine: sa.engine.Engine) -> bool:
     """Returns True if `engine` wraps a SQLite connection, and False otherwise."""
 
     return engine.dialect.name == 'sqlite'
 
-def isInMemoryEngine(engine: _sa.engine.Engine) -> bool:
+def isInMemoryEngine(engine: sa.engine.Engine) -> bool:
     """Returns True if `engine` wraps a SQLite connection working in memory,
     and False otherwise.
     """
 
     return isSqliteEngine(engine) and not bool(engine.url.database)
 
-def isFileEngine(engine: _sa.engine.Engine) -> bool:
+def isFileEngine(engine: sa.engine.Engine) -> bool:
     """Returns True if `engine` wraps a SQLite connection associated with
     a file, and False otherwise."""
 
--- investint/database/session.py
+++ investint/database/session.py
@@ -1,7 +1,14 @@
-import sqlalchemy     as _sa
-import sqlalchemy.orm as _sa_orm
+import sqlalchemy     as sa
+import sqlalchemy.orm as sa_orm
 
-metadata        = _sa.MetaData()
-mapper_registry = _sa_orm.registry(metadata)
-session_factory = _sa_orm.sessionmaker()
-Session         = _sa_orm.scoped_session(session_factory)
+__all__ = [
+    'metadata',
+    'mapper_registry',
+    'session_factory',
+    'Session'
+]
+
+metadata        = sa.MetaData()
+mapper_registry = sa_orm.registry(metadata)
+session_factory = sa_orm.sessionmaker()
+Session         = sa_orm.scoped_session(session_factory)
